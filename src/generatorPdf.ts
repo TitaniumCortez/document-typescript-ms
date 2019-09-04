9278b870c9f0c3f43d3bf2e108ad749dabf7c652
RenanCortez <renancortez141@gmail.com>
2019-09-04 01:15:19 -0300

    teste lib pdfkit

---
+++ src/generatorPdf.ts
@@ -0,0 +1,56 @@
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+export default class GeneratorPdf {
+
+     doc = new PDFDocument();
+
+    printText() {
+        // Create a document
+        
+
+        // Pipe its output somewhere, like to a file or HTTP response
+        // See below for browser usage
+        this.doc.pipe(fs.createWriteStream('output.pdf'));
+
+       
+
+        // Add another page
+        this.doc.addPage()
+            .fontSize(25)
+            .text('Here is some vector graphics...', 100, 100);
+
+        // Draw a triangle
+        this.doc.save()
+            .moveTo(100, 150)
+            .lineTo(100, 250)
+            .lineTo(200, 250)
+            .fill("#FF3300");
+
+        // Apply some transforms and render an SVG path with the 'even-odd' fill rule
+        this.doc.scale(0.6)
+            .translate(470, -380)
+            .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+            .fill('red', 'even-odd')
+            .restore();
+
+        // Add some text with annotations
+        this.doc.addPage()
+            .fillColor("blue")
+            .text('Here is a link!', 100, 100)
+            .underline(100, 100, 160, 27, { color: "#0000FF" })
+            .link(100, 100, 160, 27, 'http://google.com/');
+
+       
+    }
+    salve() {
+       // Finalize PDF file
+       this.doc.end();
+
+    }
+
+}
+
+const pdf = new GeneratorPdf();
+pdf.printText();
+pdf.salve();
